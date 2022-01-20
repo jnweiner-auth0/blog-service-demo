9b4688365385f1c00131950faf49c0166911b30d
Jordan Weiner <jordan.weiner@auth0.com>
2022-01-20 11:28:55 -0800

    fix: clean up tests

--- server/server_test.go
+++ server/server_test.go
@@ -5,93 +5,247 @@ import (
 	"blog-service/server"
 	"blog-service/server/mock"
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 )
 
-func TestServer(t *testing.T) {
+/*
+for reference:
+gomock docs: https://github.com/golang/mock
+gomock tutorial: https://gist.github.com/thiagozs/4276432d12c2e5b152ea15b3f8b0012e
+*/
 
-	// best way to set up a before each?
-	mockCtrl := gomock.NewController(t)
+var mockCtrl *gomock.Controller
+var mockDBClient *mock.MockDBClient
+var testServer *server.Server
+
+func beforeEach(t *testing.T) {
+	mockCtrl = gomock.NewController(t)
+	mockDBClient = mock.NewMockDBClient(mockCtrl)
+	testServer = server.NewServer(mockDBClient)
+}
+
+func TestCreateBlog(t *testing.T) {
+	beforeEach(t)
+	// mockCtrl.Finish() will assert the expectation statements
 	defer mockCtrl.Finish()
 
-	mockDBClient := mock.NewMockDBClient(mockCtrl)
+	testReq := &blogProto.CreateBlogRequest{
+		Title:   "Test title",
+		Content: "Test content",
+	}
 
-	testServer := server.NewServer(mockDBClient)
-
-	t.Run("CreateBlog() calls DBClient.CreateBlog() with expected data", func(t *testing.T) {
-		testBlog := &blogProto.CreateBlogRequest{
-			Title:   "Test title",
-			Content: "Test content",
-		}
-		testResult := &blogProto.CreateBlogResponse{
+	t.Run("calls DB with given data and returns expected response", func(t *testing.T) {
+		testResp := &blogProto.CreateBlogResponse{
 			Id:      "1",
 			Title:   "Test title",
 			Content: "Test content",
 		}
-		mockDBClient.EXPECT().CreateBlog(testBlog).Return(testResult, nil).Times(1)
 
-		testServer.CreateBlog(context.Background(), testBlog)
+		mockDBClient.EXPECT().CreateBlog(testReq).Return(testResp, nil).Times(1)
+
+		resp, err := testServer.CreateBlog(context.Background(), testReq)
+		if err != nil {
+			t.Fatalf("unexpected error in DB call: %q", err)
+		}
+		if want, got := testResp, resp; want != got {
+			t.Fatalf("wanted response: %q, got response: %q", want, got)
+		}
 	})
 
-	t.Run("DeleteBlog() calls DBClient.DeleteBlog() with expected data", func(t *testing.T) {
-		testBlog := &blogProto.DeleteBlogRequest{
+	t.Run("returns error if DB call fails", func(t *testing.T) {
+		dbError := errors.New("Mock DB error")
+
+		mockDBClient.EXPECT().CreateBlog(testReq).Return(nil, dbError).Times(1)
+
+		resp, err := testServer.CreateBlog(context.Background(), testReq)
+		if resp != nil {
+			t.Fatalf("unexpected resp on failed DB call: %q", resp)
+		}
+		if want, got := err, dbError; want != got {
+			t.Fatalf("wanted error: %q, got error: %q", want, got)
+		}
+	})
+}
+
+func TestDeleteBlog(t *testing.T) {
+	beforeEach(t)
+	defer mockCtrl.Finish()
+
+	testReq := &blogProto.DeleteBlogRequest{
+		Id: "1",
+	}
+
+	t.Run("calls DB with given data and returns expected response", func(t *testing.T) {
+		testResp := &blogProto.DeleteBlogResponse{
 			Id: "1",
 		}
-		testResult := &blogProto.DeleteBlogResponse{
-			Id: "1",
+
+		mockDBClient.EXPECT().DeleteBlog(testReq).Return(testResp, nil).Times(1)
+
+		resp, err := testServer.DeleteBlog(context.Background(), testReq)
+		if err != nil {
+			t.Fatalf("unexpected error in DB call: %q", err)
 		}
-		mockDBClient.EXPECT().DeleteBlog(testBlog).Return(testResult, nil).Times(1)
-		testServer.DeleteBlog(context.TODO(), testBlog)
+		if want, got := testResp, resp; want != got {
+			t.Fatalf("wanted response: %q, got response: %q", want, got)
+		}
 	})
 
-	t.Run("UpdateBlog() calls DBClient.UpdateBlog() with expected data", func(t *testing.T) {
-		testBlog := &blogProto.UpdateBlogRequest{
+	t.Run("returns error if DB call fails", func(t *testing.T) {
+		dbError := errors.New("Mock DB error")
+
+		mockDBClient.EXPECT().DeleteBlog(testReq).Return(nil, dbError).Times(1)
+
+		resp, err := testServer.DeleteBlog(context.Background(), testReq)
+		if resp != nil {
+			t.Fatalf("unexpected resp on failed DB call: %q", resp)
+		}
+		if want, got := err, dbError; want != got {
+			t.Fatalf("wanted error: %q, got error: %q", want, got)
+		}
+	})
+}
+
+func TestUpdateBlog(t *testing.T) {
+	beforeEach(t)
+	defer mockCtrl.Finish()
+
+	testReq := &blogProto.UpdateBlogRequest{
+		Id:      "1",
+		Title:   "Test title",
+		Content: "Test content",
+	}
+
+	t.Run("calls DB with given data and returns expected response", func(t *testing.T) {
+		testResp := &blogProto.UpdateBlogResponse{
 			Id:      "1",
 			Title:   "Test title",
 			Content: "Test content",
 		}
-		testResult := &blogProto.UpdateBlogResponse{
+
+		mockDBClient.EXPECT().UpdateBlog(testReq).Return(testResp, nil).Times(1)
+
+		resp, err := testServer.UpdateBlog(context.Background(), testReq)
+		if err != nil {
+			t.Fatalf("unexpected error in DB call: %q", err)
+		}
+		if want, got := testResp, resp; want != got {
+			t.Fatalf("wanted response: %q, got response: %q", want, got)
+		}
+	})
+
+	t.Run("returns error if DB call fails", func(t *testing.T) {
+		dbError := errors.New("Mock DB error")
+
+		mockDBClient.EXPECT().UpdateBlog(testReq).Return(nil, dbError).Times(1)
+
+		resp, err := testServer.UpdateBlog(context.Background(), testReq)
+		if resp != nil {
+			t.Fatalf("unexpected resp on failed DB call: %q", resp)
+		}
+		if want, got := err, dbError; want != got {
+			t.Fatalf("wanted error: %q, got error: %q", want, got)
+		}
+	})
+}
+
+func TestGetBlog(t *testing.T) {
+	beforeEach(t)
+	defer mockCtrl.Finish()
+
+	testReq := &blogProto.GetBlogRequest{
+		Id: "1",
+	}
+
+	t.Run("calls DB with given data and returns expected response", func(t *testing.T) {
+		testResp := &blogProto.GetBlogResponse{
 			Id:      "1",
 			Title:   "Test title",
 			Content: "Test content",
 		}
-		mockDBClient.EXPECT().UpdateBlog(testBlog).Return(testResult, nil).Times(1)
-		testServer.UpdateBlog(context.Background(), testBlog)
+
+		mockDBClient.EXPECT().GetBlog(testReq).Return(testResp, nil).Times(1)
+
+		resp, err := testServer.GetBlog(context.Background(), testReq)
+		if err != nil {
+			t.Fatalf("unexpected error in DB call: %q", err)
+		}
+		if want, got := testResp, resp; want != got {
+			t.Fatalf("wanted response: %q, got response: %q", want, got)
+		}
 	})
 
-	t.Run("GetBlog() calls DBClient.GetBlog() with expected data", func(t *testing.T) {
-		testBlog := &blogProto.GetBlogRequest{
-			Id: "1",
-		}
-		testResult := &blogProto.GetBlogResponse{
-			Id:      "1",
-			Title:   "Test title",
-			Content: "Test content",
-		}
-		mockDBClient.EXPECT().GetBlog(testBlog).Return(testResult, nil).Times(1)
-		testServer.GetBlog(context.Background(), testBlog)
-	})
+	t.Run("returns error if DB call fails", func(t *testing.T) {
+		dbError := errors.New("Mock DB error")
 
-	t.Run("ListBlog() calls DBClient.ListBlog() with given Limit if Limit provided", func(t *testing.T) {
-		testBlogWithLimit := &blogProto.ListBlogRequest{
-			Limit: 10,
+		mockDBClient.EXPECT().GetBlog(testReq).Return(nil, dbError).Times(1)
+
+		resp, err := testServer.GetBlog(context.Background(), testReq)
+		if resp != nil {
+			t.Fatalf("unexpected resp on failed DB call: %q", resp)
 		}
-		testResult := &blogProto.ListBlogResponse{
+		if want, got := err, dbError; want != got {
+			t.Fatalf("wanted error: %q, got error: %q", want, got)
+		}
+	})
+}
+
+func TestListBlog(t *testing.T) {
+	beforeEach(t)
+	defer mockCtrl.Finish()
+
+	testReq := &blogProto.ListBlogRequest{
+		Limit: 10,
+	}
+
+	testReqNoLimit := &blogProto.ListBlogRequest{}
+
+	t.Run("calls DB with given Limit (if Limit provided) and returns expected response", func(t *testing.T) {
+		testResp := &blogProto.ListBlogResponse{
 			Blogs: []*blogProto.CreateBlogResponse{},
 		}
-		mockDBClient.EXPECT().ListBlog(testBlogWithLimit).Return(testResult, nil).Times(1)
-		testServer.ListBlog(context.Background(), testBlogWithLimit)
+
+		mockDBClient.EXPECT().ListBlog(testReq).Return(testResp, nil).Times(1)
+
+		resp, err := testServer.ListBlog(context.Background(), testReq)
+		if err != nil {
+			t.Fatalf("unexpected error in DB call: %q", err)
+		}
+		if want, got := testResp, resp; want != got {
+			t.Fatalf("wanted response: %q, got response: %q", want, got)
+		}
 	})
 
-	t.Run("ListBlog() calls DBClient.ListBlog() with Limit: 25 if no Limit provided", func(t *testing.T) {
-		testBlogWithoutLimit := &blogProto.ListBlogRequest{}
-		testResult := &blogProto.ListBlogResponse{
+	t.Run("calls DB with Limit: 25 (if no Limit provided) and returns expected response", func(t *testing.T) {
+		testResp := &blogProto.ListBlogResponse{
 			Blogs: []*blogProto.CreateBlogResponse{},
 		}
-		mockDBClient.EXPECT().ListBlog(&blogProto.ListBlogRequest{Limit: 25}).Return(testResult, nil).Times(1)
-		testServer.ListBlog(context.Background(), testBlogWithoutLimit)
+
+		mockDBClient.EXPECT().ListBlog(&blogProto.ListBlogRequest{Limit: 25}).Return(testResp, nil).Times(1)
+
+		resp, err := testServer.ListBlog(context.Background(), testReqNoLimit)
+		if err != nil {
+			t.Fatalf("unexpected error in DB call: %q", err)
+		}
+		if want, got := testResp, resp; want != got {
+			t.Fatalf("wanted response: %q, got response: %q", want, got)
+		}
 	})
 
+	t.Run("returns error if DB call fails", func(t *testing.T) {
+		dbError := errors.New("Mock DB error")
+
+		mockDBClient.EXPECT().ListBlog(testReq).Return(nil, dbError).Times(1)
+
+		resp, err := testServer.ListBlog(context.Background(), testReq)
+		if resp != nil {
+			t.Fatalf("unexpected resp on failed DB call: %q", resp)
+		}
+		if want, got := err, dbError; want != got {
+			t.Fatalf("wanted error: %q, got error: %q", want, got)
+		}
+	})
 }
